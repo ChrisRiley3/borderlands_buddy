c03a0b217d4af57fd8413767bab3ce7cabcdc3fb
Christopher Riley <chris_riley1993@hotmail.com>
2020-08-17 12:58:29 +0000

    Added weapon details functionality

--- weapons/urls.py
+++ weapons/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_weapons, name='weapons')
+    path('', views.all_weapons, name='weapons'),
+    path('<weapon_id>', views.weapon_detail, name='weapon_detail'),
+
 ]
--- weapons/views.py
+++ weapons/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Weapon
 
 
@@ -12,3 +12,15 @@ def all_weapons(request):
     }
 
     return render(request, 'weapons/weapons.html', context)
+
+
+def weapon_detail(request, weapon_id):
+    """ A view to show individual weapon details """
+
+    weapon = get_object_or_404(Weapon, pk=weapon_id)
+
+    context = {
+        'weapon': weapon,
+    }
+
+    return render(request, 'weapons/weapon_detail.html', context)
