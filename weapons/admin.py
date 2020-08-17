09a0397a9c15467d7a95948260f99a76fccd3dd2
Christopher Riley <chris_riley1993@hotmail.com>
2020-08-17 09:31:15 +0000

    Customized the admin page

--- weapons/admin.py
+++ weapons/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Weapon, Category
 
 
-admin.site.register(Weapon)
-admin.site.register(Category)
+class weaponAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'manufacture',
+        'price',
+        'category',
+        'image',
+    )
+
+
+class categoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Weapon, weaponAdmin)
+admin.site.register(Category, categoryAdmin)
