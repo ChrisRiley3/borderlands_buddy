dbbc53356b5f192e33535feda88037358c7319db
Christopher Riley <chris_riley1993@hotmail.com>
2020-08-17 09:49:30 +0000

    Added weapons views and templates

--- weapons/views.py
+++ weapons/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Weapon
 
-# Create your views here.
+
+def all_weapons(request):
+    """ A view to show all the weapons, including sorting and search queries  """
+
+    weapons = Weapon.objects.all()
+
+    context = {
+        'weapons': weapons,
+    }
+
+    return render(request, 'weapons/weapons.html', context)
