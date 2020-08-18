4f5f5b2be37e34e130a251ae8c94e3d0f59144fb
Christopher Riley <chris_riley1993@hotmail.com>
2020-08-18 17:02:30 +0000

    Added search functionality

--- weapons/urls.py
+++ weapons/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.all_weapons, name='weapons'),
     path('<weapon_id>', views.weapon_detail, name='weapon_detail'),
-
 ]
--- weapons/views.py
+++ weapons/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Weapon
 
 
@@ -6,9 +8,21 @@ def all_weapons(request):
     """ A view to show all the weapons, including sorting and search queries  """
 
     weapons = Weapon.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            weapons = weapons.filter(queries)
 
     context = {
         'weapons': weapons,
+        'search_term': query,
     }
 
     return render(request, 'weapons/weapons.html', context)
