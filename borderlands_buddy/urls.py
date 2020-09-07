8a088a8750b535d7ccd9cde06b83b8784feab7c3
Christopher Riley <chris_riley1993@hotmail.com>
2020-09-07 19:04:20 +0000

    Added checkout views and templates

--- borderlands_buddy/urls.py
+++ borderlands_buddy/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('weapons/', include('weapons.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
