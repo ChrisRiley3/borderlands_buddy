411e817f6dc317b4a5909f1850b84ca73f4a904e
Christopher Riley <chris_riley1993@hotmail.com>
2020-09-25 10:23:40 +0000

    Added add weapons view, url and template

--- weapons/urls.py
+++ weapons/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_weapons, name='weapons'),
-    path('<weapon_id>', views.weapon_detail, name='weapon_detail'),
+    path('<int:weapon_id>/', views.weapon_detail, name='weapon_detail'),
+    path('add/', views.add_weapon, name='add_weapon'),
 ]
--- weapons/views.py
+++ weapons/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Weapon, Category
 from django.db.models.functions import Lower
+
+from .models import Weapon, Category
+from .forms import WeaponForm
 
 
 def all_weapons(request):
@@ -65,3 +67,14 @@ def weapon_detail(request, weapon_id):
     }
 
     return render(request, 'weapons/weapon_detail.html', context)
+
+
+def add_weapon(request):
+    """ Add a product to the store """
+    form = WeaponForm()
+    template = 'weapons/add_weapon.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
