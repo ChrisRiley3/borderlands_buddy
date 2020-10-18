965ccefe88dad56f266c36d5ef7e36c6d65282b2
Christopher Riley <chris_riley1993@hotmail.com>
2020-10-18 13:20:09 +0000

    Updated the views to show feedback written in the admin

--- feedback/models.py
+++ feedback/models.py
@@ -7,4 +7,4 @@ class Feedback(models.Model):
     feedback = models.CharField(max_length=500, null=False, blank=False)
 
     def __str__(self):
-        return self.name
+        return self.feedback
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Feedback
 
 
 def feedback(request):
     """ A view to render the feedback page """
-    return render(request, 'feedback/feedback.html')
+    feedbacks = Feedback.objects.all()
+    context = {
+        'feedbacks': feedbacks
+    }
+    return render(request, 'feedback/feedback.html', context)
