b90b268abc1754334b228f921c4245978583f849
Christopher Riley <chris_riley1993@hotmail.com>
2020-12-13 13:35:02 +0000

    Updated Weapons Model in weapons app so all fields are required when adding/editing a weapon

--- weapons/models.py
+++ weapons/models.py
@@ -18,13 +18,13 @@ class Category(models.Model):
 
 
 class Weapon(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=False, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    manufacture = models.CharField(max_length=254, null=True, blank=True)
+    manufacture = models.CharField(max_length=254, null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
 
     def __str__(self):
         return self.name
