07219d796e5b8b9295737e0ff2d97c1e177030e3
Christopher Riley <chris_riley1993@hotmail.com>
2020-12-14 17:39:29 +0000

    Added test_forms.py to profiles app and tested forms.py

---
+++ profiles/test_forms.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from .forms import UserProfileForm
+
+
+class TestUserProfileForm(TestCase):
+
+    def test_default_phone_number_is_not_required(self):
+        form = UserProfileForm({'default_phone_number': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_street_address1_is_not_required(self):
+        form = UserProfileForm({'default_street_address1': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_street_address2_is_not_required(self):
+        form = UserProfileForm({'default_street_address2': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_town_or_city_is_not_required(self):
+        form = UserProfileForm({'default_town_or_city': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_county_is_not_required(self):
+        form = UserProfileForm({'default_county': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_postcode_is_not_required(self):
+        form = UserProfileForm({'default_postcode': ''})
+        self.assertTrue(form.is_valid())
+
+    def test_default_country_is_not_required(self):
+        form = UserProfileForm({'default_country': ''})
+        self.assertTrue(form.is_valid())
