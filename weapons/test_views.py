17f438787f5e4c2ebe14a1fdab6a8d0e6ade211f
Christopher Riley <chris_riley1993@hotmail.com>
2020-12-16 18:04:38 +0000

    Added more testing to feedback views and weapons views

--- weapons/test_views.py
+++ weapons/test_views.py
@@ -7,3 +7,7 @@ class TestViews(TestCase):
         response = self.client.get('/weapons/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'weapons/weapons.html')
+
+    def test_can_add_review(self):
+        response = self.client.post('/weapons/add/', {'weapon': 'test', 'full_name': 'Test Name', 'email': 'test@test.com', 'feedback': 'test feedback'})
+        self.assertRedirects(response, '/accounts/login/?next=/weapons/add/')
