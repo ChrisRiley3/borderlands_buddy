1545362a9d689b6f756be4c9df88876bd5d9f741
Christopher Riley <chris_riley1993@hotmail.com>
2020-12-21 20:17:29 +0000

    removed secret key and set debug

--- borderlands_buddy/settings.py
+++ borderlands_buddy/settings.py
@@ -9,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!c!2%g0e^4sj*$k%w7#7cozk0g&=u+ld5zp0h^)82)9yne&7@c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['borderlands-buddy.herokuapp.com', 'localhost']
 
